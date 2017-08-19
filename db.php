bdad1be464d4d4995ec33e71bbf787125cf35649
beketovgreen <beketovgreen@gmail.com>
2017-08-19 03:09:43 +0300

    first commit

--- db.php
+++ db.php
@@ -2,17 +2,9 @@
 require_once 'dblogin.php';
 
 try{
-    $pdo = PDO('mysql:host='+$db_localhost+';dbname='+$db_database, $db_username, $db_password );
+    $pdo = PDO('mysql:host=localhost;dbname=crud', $db_username, $db_password );
 
 }catch (PDOException $e){
     echo "Connecting fall";
 }
-$page = isset($_GET['add'])?$_GET['add']:'';
-if ($page == 'add'){
-
-
-
-}
-
-
 ?>
